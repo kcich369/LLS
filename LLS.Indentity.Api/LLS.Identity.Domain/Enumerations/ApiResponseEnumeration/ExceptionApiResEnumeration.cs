397c4dce6ea2b158b05938745f2c43a9e0ab6d05
Dragonet <kcich369@gmail.com>
2024-03-17 23:18:21 +0100

    changes

--- LLS.Indentity.Api/LLS.Identity.Domain/Enumerations/ApiResponseEnumeration/ExceptionApiResEnumeration.cs
+++ LLS.Indentity.Api/LLS.Identity.Domain/Enumerations/ApiResponseEnumeration/ExceptionApiResEnumeration.cs
@@ -4,11 +4,10 @@ namespace LLS.Identity.Domain.Enumerations.ApiResponseEnumeration;
 
 public class ExceptionApiResEnumeration : ApiResponseTypesEnumerations
 {
-    public ExceptionApiResEnumeration Exception = new(0, nameof(Exception), StatusCodeEnumerations.InternalServerError);
+    public ExceptionApiResEnumeration Exception = new(0, nameof(Exception));
 
-    protected ExceptionApiResEnumeration(int errorCode, string errorMessageTranslation,
-        StatusCodeEnumerations statusCode) : base(500, "Internal server error", errorCode, errorMessageTranslation,
-        statusCode)
+    protected ExceptionApiResEnumeration(int errorCode, string errorMessage)
+        : base(500, "Internal server error", errorCode, errorMessage, StatusCodeEnumerations.InternalServerError)
     {
     }
 }
